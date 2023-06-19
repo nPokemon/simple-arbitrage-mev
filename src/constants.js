0e1b5e68cf05621e493857102f655219f57654be
edwardmolasses <alachkovics@gmail.com>
2023-06-19 10:11:24 -0700

    add constants file

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+// constants.js
+export const ALCHEMY_RPC_URL = 'https://eth-mainnet.g.alchemy.com/v2/4D8EexfXmKF7f4rNQ0IE9SdTJDqu-Dgy';
+export const UNISWAP_ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+export const sushiswapAddress = '0x397FF1542f962076d0BFE58eA045FfA2d347ACa0';
